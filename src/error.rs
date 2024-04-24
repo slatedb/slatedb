b558cbd7f5d3924538881b91a3ffec041360271f
Paul Butler <paul@driftingin.space>
2024-04-24 14:28:31 -0400

    add error.rs

---
+++ src/error.rs
@@ -0,0 +1,14 @@
+#[derive(thiserror::Error, Debug)]
+pub enum SlateDBError {
+    #[error("IO error")]
+    IoError(#[from] std::io::Error),
+
+    #[error("Checksum mismatch")]
+    ChecksumMismatch,
+
+    #[error("Empty block meta")]
+    EmptyBlockMeta,
+
+    #[error("Object store error")]
+    ObjectStoreError(#[from] object_store::Error),
+}
