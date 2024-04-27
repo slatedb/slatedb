e715c975c00b3fbce4ac52ae69717b492985fc3d
Paul Butler <paul@driftingin.space>
2024-04-26 21:12:19 -0400

    Add comment on KeyValueIterator

--- src/iter.rs
+++ src/iter.rs
@@ -6,6 +6,10 @@ pub struct KeyValue {
     pub value: Bytes,
 }
 
+/// Note: this is intentionally its own trait instead of an Iterator<Item=KeyValue>,
+/// because next will need to be made async to support SSTs, which are loaded over
+/// the network.
+/// See: https://github.com/slatedb/slatedb/issues/12
 pub trait KeyValueIterator {
     fn next(&mut self) -> Option<KeyValue>;
 }
