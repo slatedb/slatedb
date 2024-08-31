ddd1caf1f9b06191641625de10339e47ed26bf89
Li Yazhou <me.ssword@gmail.com>
2024-08-31 21:26:42 +0800

    add cache about attributes

--- src/disk_cache.rs
+++ src/disk_cache.rs
@@ -5,7 +5,7 @@ use futures::stream;
 use futures::stream::BoxStream;
 use futures::StreamExt;
 use object_store::buffered::BufWriter;
-use object_store::local;
+use object_store::Attribute;
 use object_store::Attributes;
 use object_store::GetRange;
 use object_store::GetResultPayload;
@@ -458,7 +458,7 @@ impl LocalCacheHead {
     pub fn meta(&self) -> ObjectMeta {
         ObjectMeta {
             location: self.location.clone().into(),
-            last_modified: self.last_modified.parse().unwrap(),
+            last_modified: self.last_modified.parse().unwrap_or_default(),
             size: self.size,
             e_tag: self.e_tag.clone(),
             version: self.version.clone(),
@@ -466,19 +466,45 @@ impl LocalCacheHead {
     }
 
     pub fn attributes(&self) -> Attributes {
-        todo!()
+        let mut attrs = Attributes::new();
+        for (key, value) in self.attributes.iter() {
+            let key = match key.as_str() {
+                "Cache-Control" => Attribute::CacheControl,
+                "Content-Disposition" => Attribute::ContentDisposition,
+                "Content-Encoding" => Attribute::ContentEncoding,
+                "Content-Language" => Attribute::ContentLanguage,
+                "Content-Type" => Attribute::ContentType,
+                _ => Attribute::Metadata(key.to_string().into()),
+            };
+            let value = value.to_string().into();
+            attrs.insert(key, value);
+        }
+        attrs
     }
 }
 
 impl From<(&ObjectMeta, &Attributes)> for LocalCacheHead {
     fn from((meta, attrs): (&ObjectMeta, &Attributes)) -> Self {
+        let mut attrs_map = HashMap::new();
+        for (key, value) in attrs.iter() {
+            let key = match key {
+                Attribute::CacheControl => "Cache-Control",
+                Attribute::ContentDisposition => "Content-Disposition",
+                Attribute::ContentEncoding => "Content-Encoding",
+                Attribute::ContentLanguage => "Content-Language",
+                Attribute::ContentType => "Content-Type",
+                Attribute::Metadata(key) => key,
+                _ => continue,
+            };
+            attrs_map.insert(key.to_string(), value.to_string());
+        }
         LocalCacheHead {
             location: meta.location.to_string(),
             last_modified: meta.last_modified.to_rfc3339(),
             size: meta.size,
             e_tag: meta.e_tag.clone(),
             version: meta.version.clone(),
-            attributes: todo!(),
+            attributes: attrs_map,
         }
     }
 }
