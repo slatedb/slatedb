04a872b3e368b70a5af777d8d13287b63bb3de51
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2024-10-03 00:44:42 -0400

    Add simple benchmarks to BloomFilterBuilder
    There are benchmarks for normal bloom filter operations
    (building, querying, and insertion). However, the main benchmark of
    interest in this commit is bloom_filter_insert_benchmark, as this
    allows us to stress filter_hash(). As the flmaegraph indicates, we
    spend roughly 12% of benchmark execution time in filter_hash().
    
    The hope is to reduce this in subsequent PRs/patches

--- benches/bloom_bench.rs
+++ benches/bloom_bench.rs
@@ -1,17 +1,21 @@
-use bytes::{BufMut, BytesMut};
+use bytes::{BufMut, Bytes, BytesMut};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
 use slatedb::filter::{filter_hash, BloomFilterBuilder};
+fn make_key(key_size: usize, element: u32) -> Bytes {
+    // Pre-generate the keys
+    let mut bytes = BytesMut::with_capacity(key_size);
+    bytes.put_u32(element);
+    bytes.clone().freeze();
+    bytes.into()
+}
 fn bloom_filter_insert_benchmark(c: &mut Criterion) {
     let mut builder = BloomFilterBuilder::new(10); // Set bits_per_key
     let key_sz = std::mem::size_of::<u32>();
-
+    let bytes = make_key(key_sz, 1u32);
     c.bench_function("bloom_filter insert 10000000 keys", |b| {
         b.iter(|| {
-            for i in 0..10000000 {
-                let mut bytes = BytesMut::with_capacity(key_sz);
-                bytes.put_u32(i);
-                builder.add_key(black_box(bytes.freeze().as_ref())); // Use `black_box` to prevent compiler optimizations
-            }
+            builder.add_key(black_box(bytes.as_ref()));
         })
     });
 }
@@ -19,16 +23,11 @@ fn bloom_filter_build_benchmark(c: &mut Criterion) {
     let mut builder = BloomFilterBuilder::new(10);
     let key_sz = std::mem::size_of::<u32>();
 
-    // Add keys to builder before measuring build time
-    for i in 0..100000 {
-        let mut bytes = BytesMut::with_capacity(key_sz);
-        bytes.put_u32(i);
-        builder.add_key(bytes.freeze().as_ref());
-    }
-
+    let bytes = make_key(key_sz, 1u32);
+    builder.add_key(black_box(bytes.as_ref()));
     c.bench_function("bloom_filter build", |b| {
         b.iter(|| {
-            let _filter = black_box(builder.build());
+            black_box(builder.build());
         })
     });
 }
@@ -36,29 +35,34 @@ fn bloom_filter_query_benchmark(c: &mut Criterion) {
     let mut builder = BloomFilterBuilder::new(10);
     let key_sz = std::mem::size_of::<u32>();
 
-    for i in 0..100000 {
-        let mut bytes = BytesMut::with_capacity(key_sz);
-        bytes.put_u32(i);
-        builder.add_key(bytes.freeze().as_ref());
-    }
+    let bytes = make_key(key_sz, 1u32);
+    builder.add_key(black_box(bytes.as_ref()));
     let filter = builder.build();
 
     c.bench_function("bloom_filter query", |b| {
         b.iter(|| {
-            for i in 0..100000 {
-                let mut bytes = BytesMut::with_capacity(key_sz);
-                bytes.put_u32(i);
-                let hash = filter_hash(bytes.freeze().as_ref());
-                black_box(filter.might_contain(hash));
-            }
+            let hash = filter_hash(bytes.as_ref());
+            black_box(filter.might_contain(hash));
         })
     });
 }
 
+fn bloom_build_and_insert_benchmark(c: &mut Criterion) {
+    let mut builder = BloomFilterBuilder::new(10); // Set bits_per_key
+    let key_sz = std::mem::size_of::<u32>();
+    let bytes = make_key(key_sz, 1u32);
+    c.bench_function("bloom_filter build and insert", |b| {
+        b.iter(|| {
+            builder.add_key(black_box(bytes.as_ref()));
+            black_box(builder.build());
+        })
+    });
+}
 criterion_group!(
     bloom_benchmarks,
     bloom_filter_insert_benchmark,
-    // bloom_filter_build_benchmark,
-    // bloom_filter_query_benchmark
+    bloom_filter_build_benchmark,
+    bloom_filter_query_benchmark,
+    bloom_build_and_insert_benchmark
 );
 criterion_main!(bloom_benchmarks);
