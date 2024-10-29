62e3a032b74e66c0e2227fae6547446c3d6c4816
KaguraMilet <asukananase810@gmail.com>
2024-10-29 22:06:46 +0800

    add tests for ghost queue

--- src/cached_object_store/ghost.rs
+++ src/cached_object_store/ghost.rs
@@ -45,10 +45,21 @@ impl<K: Hash + Eq, V> GhostQueue<K, V> {
         })
     }
 
+    #[allow(dead_code)]
+    pub fn get(&mut self, key: &K) -> Option<&V> {
+        self.queue.get(key)
+    }
+
     #[inline]
     #[allow(dead_code)]
     pub fn usage(&self) -> usize {
         self.usage
+    }
+
+    #[inline]
+    #[allow(dead_code)]
+    pub fn len(&self) -> usize {
+        self.queue.len()
     }
 
     #[inline]
@@ -92,5 +103,94 @@ impl<K: Hash + Eq + 'static, V: 'static> GhostBuilder<K, V> {
 
     pub fn build(self) -> GhostQueue<K, V> {
         GhostQueue::new(self.capacity, self.weighter)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        fmt::Debug,
+        sync::atomic::{AtomicUsize, Ordering},
+    };
+
+    fn assert_opt_eq<V: PartialEq + Debug>(opt: Option<&V>, v: V) {
+        assert!(opt.is_some());
+        assert_eq!(opt.unwrap(), &v);
+    }
+
+    #[test]
+    fn test_record() {
+        let mut ghost = GhostBuilder::new(NonZeroUsize::new(2).unwrap()).build();
+        ghost.record("Saito", "Asuka");
+        ghost.record("Nishino", "Nanase");
+        assert_eq!(ghost.usage(), 2);
+        assert_eq!(ghost.capacity().get(), 2);
+        assert_opt_eq(ghost.get(&"Saito"), "Asuka");
+        assert_opt_eq(ghost.get(&"Nishino"), "Nanase");
+    }
+
+    #[test]
+    fn test_remove() {
+        let mut ghost = GhostBuilder::new(NonZeroUsize::new(2).unwrap()).build();
+        ghost.record("Saito", "Asuka");
+        ghost.record("Nishino", "Nanase");
+        assert_opt_eq(ghost.remove(&"Nishino").as_ref(), ("Nishino", "Nanase"));
+        assert_eq!(ghost.usage(), 1);
+        assert!(ghost.get(&"Nishino").is_none());
+        assert_opt_eq(ghost.get(&"Saito"), "Asuka");
+    }
+
+    #[test]
+    fn test_weighter() {
+        let mut cache = GhostBuilder::new(NonZeroUsize::new(10).unwrap())
+            .with_weighter(|k: &Vec<i32>, v: &Vec<i32>| k.len() + v.len())
+            .build();
+
+        cache.record(vec![1, 2, 3], vec![4, 5, 6]);
+        // Current usage should be 6
+        assert_eq!(cache.len(), 1);
+        assert_eq!(cache.usage(), 6);
+        // Cache internal should trigger eviction and <vec![1, 2, 3], vec![4, 5, 6]> is not exist
+        cache.record(vec![7, 8, 9], vec![10, 11, 12]);
+        assert_eq!(cache.len(), 1);
+        assert_eq!(cache.usage(), 6);
+        assert!(cache.get(&vec![1, 2, 3]).is_none());
+    }
+
+    #[test]
+    fn test_send() {
+        use std::thread;
+
+        let mut ghost = GhostBuilder::new(NonZeroUsize::new(3).unwrap()).build();
+        ghost.record("Erik ten Hag", "sacked");
+
+        let handle = thread::spawn(move || {
+            assert_eq!(ghost.get(&"Erik ten Hag"), Some(&"sacked"));
+        });
+
+        assert!(handle.join().is_ok());
+    }
+
+    #[test]
+    fn test_no_memory_leaks() {
+        static DROP_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+        struct DropCounter;
+
+        impl Drop for DropCounter {
+            fn drop(&mut self) {
+                DROP_COUNT.fetch_add(1, Ordering::SeqCst);
+            }
+        }
+
+        let n = 100;
+        for _ in 0..n {
+            let mut ghost = GhostBuilder::new(NonZeroUsize::new(1).unwrap()).build();
+            for i in 0..n {
+                ghost.record(i, DropCounter {});
+            }
+        }
+        assert_eq!(DROP_COUNT.load(Ordering::SeqCst), n * n);
     }
 }
