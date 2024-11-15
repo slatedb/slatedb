8627d6d4972ea4d8ace9724a38bae883d7afc57e
David Moravek <dmvk@apache.org>
2024-11-15 16:15:08 +0100

    feat(MergeOperator): Implement RowCodec for Merge Operands.

--- src/error.rs
+++ src/error.rs
@@ -64,8 +64,12 @@ pub enum SlateDBError {
     #[error("Error Compressing Block")]
     BlockCompressionError,
 
-    #[error("Unknown RowFlags -- this may be caused by reading data encoded with a newer codec")]
-    InvalidRowFlags,
+    #[error("Invalid RowFlags (encoded_bits: {encoded_bits:#b}, known_bits: {known_bits:#b}): {message}")]
+    InvalidRowFlags {
+        encoded_bits: u8,
+        known_bits: u8,
+        message: String,
+    },
 
     #[error("Error flushing immutable wals: channel closed")]
     WalFlushChannelError,
