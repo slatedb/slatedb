a6845ccd4d113fee4d07bbaae0339c585ff0847f
Mohinish <mohinish_ce@yahoo.com>
2024-11-19 01:10:47 -0600

    fixed formatting

--- src/config.rs
+++ src/config.rs
@@ -468,7 +468,6 @@ pub struct DbOptions {
 }
 
 impl DbOptions {
-
     /// Converts the DbOptions to a JSON string representation
     pub fn to_json_string(&self) -> Result<String, serde_json::Error> {
         serde_json::to_string_pretty(self)
