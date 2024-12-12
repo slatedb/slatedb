b9ebbd82e5fd5dd86a2b2aefe8069a9059981691
Rohan <desai.p.rohan@gmail.com>
2024-12-12 14:47:34 -0800

    tighten up error handling to prevent writer clients from blocking indefinitely (#373)
    This patch tries to tighten up error handling to prevent writer clients
    from blocking indefinitely. Previously we could see:
    - a writer could pass the initial error check and then write to the in
    memory table, and then block until the table is durable. meanwhile, the
    flush task could panic or become fenced and the writer would never be
    notified.
    - a writer could block in the backpressure loop, which never clears
    because the compactor failed.
    
    The approach here consists of the following changes:
    
    First, we add some utility types/fns:
    - `WriteOnceRegister` which represents a value that can be written once.
    Later writes are dropped. The type also supports waiting for a value to
    be written to the register.
    - Utility methods for spawning monitored tasks and threads. A monitored
    task or thread is spawned by another task/thread which waits on the
    result of the monitored task/thread. Then, the monitor task/thread calls
    a cleanup fn with the result.
    
    The error field in DbImpl is removed and replaced with an error field of
    type WriteOnceRegister<SlateDBError> in DbState. We had to move this to
    DbState so that we could get mutual exclusion between setting the error
    and creating a new writable table.
    
    The flushed/durable notifications in the memtables are replaced with
    WriteOnceRegister.
    
    The wal flush task, memtable flush task, and compactor all spawn using
    the utitlity spawn methods, and also monitor the global error register.
    They signal status to the monitor task/thread by returning their status.
    The cleanup fn for the wal flush task sets the global error register,
    and the register in every wal table. The cleanup fn for the memtable
    flush task sets the global error register, and the register in every mem
    table. The cleanup fn for the comapctor sets the global error register.

--- src/compactor.rs
+++ src/compactor.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use std::sync::Arc;
-use std::thread;
 use std::thread::JoinHandle;
 use std::time::Duration;
 use std::time::{SystemTime, UNIX_EPOCH};
@@ -18,6 +17,7 @@ use crate::error::SlateDBError;
 use crate::manifest_store::{FenceableManifest, ManifestStore, StoredManifest};
 use crate::metrics::DbStats;
 use crate::tablestore::TableStore;
+use crate::utils::spawn_bg_thread;
 
 pub trait CompactionScheduler {
     fn maybe_schedule_compaction(&self, state: &CompactorState) -> Vec<Compaction>;
@@ -33,7 +33,7 @@ pub(crate) enum WorkerToOrchestratorMsg {
 
 pub(crate) struct Compactor {
     main_tx: crossbeam_channel::Sender<CompactorMainMsg>,
-    main_thread: Option<JoinHandle<()>>,
+    main_thread: Option<JoinHandle<Result<(), SlateDBError>>>,
 }
 
 impl Compactor {
@@ -43,11 +43,12 @@ impl Compactor {
         options: CompactorOptions,
         tokio_handle: Handle,
         db_stats: Arc<DbStats>,
+        cleanup_fn: impl FnOnce(&SlateDBError) + Send + 'static,
     ) -> Result<Self, SlateDBError> {
         let (external_tx, external_rx) = crossbeam_channel::unbounded();
         let (err_tx, err_rx) = tokio::sync::oneshot::channel();
         let tokio_handle = options.compaction_runtime.clone().unwrap_or(tokio_handle);
-        let main_thread = thread::spawn(move || {
+        let main_thread = spawn_bg_thread("slatedb-compactor", cleanup_fn, move || {
             let load_result = CompactorOrchestrator::new(
                 options,
                 manifest_store.clone(),
@@ -60,11 +61,11 @@ impl Compactor {
                 Ok(orchestrator) => orchestrator,
                 Err(err) => {
                     err_tx.send(Err(err)).expect("err channel failure");
-                    return;
+                    return Ok(());
                 }
             };
             err_tx.send(Ok(())).expect("err channel failure");
-            orchestrator.run();
+            orchestrator.run()
         });
         err_rx.await.expect("err channel failure")?;
         Ok(Self {
@@ -76,9 +77,10 @@ impl Compactor {
     pub(crate) async fn close(mut self) {
         if let Some(main_thread) = self.main_thread.take() {
             self.main_tx.send(Shutdown).expect("main tx disconnected");
-            main_thread
+            let result = main_thread
                 .join()
                 .expect("failed to stop main compactor thread");
+            info!("compactor thread exited with: {:?}", result)
         }
     }
 }
@@ -141,7 +143,7 @@ impl CompactorOrchestrator {
         Ok(CompactorState::new(db_state.clone()))
     }
 
-    fn run(&mut self) {
+    fn run(&mut self) -> Result<(), SlateDBError> {
         let ticker = crossbeam_channel::tick(self.options.poll_interval);
         let db_runs_log_ticker = crossbeam_channel::tick(Duration::from_secs(10));
 
@@ -172,6 +174,7 @@ impl CompactorOrchestrator {
                 }
             }
         }
+        Ok(())
     }
 
     fn load_manifest(&mut self) -> Result<(), SlateDBError> {
