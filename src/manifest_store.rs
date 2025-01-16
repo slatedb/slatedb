b9dc6aff6d3b5f3da2c52173a91fcb9cb4568938
Almog Gavra <almog.gavra@gmail.com>
2025-01-15 21:47:41 -0800

    remove println

--- src/manifest_store.rs
+++ src/manifest_store.rs
@@ -251,7 +251,6 @@ impl ManifestStore {
         id: u64,
         manifest: &Manifest,
     ) -> Result<(), SlateDBError> {
-        println!("{:?}", manifest);
         let manifest_path = &self.get_manifest_path(id);
 
         self.object_store
