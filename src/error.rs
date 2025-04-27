92794a1c0e3edcd5a5b78aaf1b38b2e5af32cfe5
krishna sindhur <krishna.sindhur@thinkbyte.ai>
2025-04-27 21:32:33 +0530

    fix: clippy error type too large

--- src/error.rs
+++ src/error.rs
@@ -151,7 +151,6 @@ pub enum DbOptionsError {
     #[error("Unknown configuration file format: {0}")]
     UnknownFormat(PathBuf),
 
-    // TODO: clippy complains this error is too large
     #[error("Invalid configuration format: {0}")]
-    InvalidFormat(#[from] figment::Error),
+    InvalidFormat(#[from] Box<figment::Error>),
 }
