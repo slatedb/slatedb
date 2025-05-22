8930ff004e9947a38b058aa2ee8541a0885fe305
Chris Riccomini <criccomini@apache.org>
2025-05-22 13:51:10 -0700

    Use rand_xoroshiro since rand is a popular library

--- slatedb/src/rand.rs
+++ slatedb/src/rand.rs
@@ -22,7 +22,8 @@
 //! let _ = with_rng(|rng| rng.u64(..));
 //! ```
 
-use fastrand::Rng;
+use rand_xoshiro::rand_core::{RngCore, SeedableRng};
+use rand_xoshiro::Xoroshiro128Plus;
 use std::cell::RefCell;
 use std::sync::{Mutex, OnceLock};
 
@@ -33,12 +34,12 @@ use crate::SlateDBError;
 // Uses `OnceLock` to ensure that the random number generator is initialized exactly once regardless
 // of which thread it was initialized on. Uses `Mutex` so we can grab a mutable reference to the
 // random number generator when forking the root RNG in a local thread.
-static ROOT_RNG: OnceLock<Mutex<Rng>> = OnceLock::new();
+static ROOT_RNG: OnceLock<Mutex<Xoroshiro128Plus>> = OnceLock::new();
 
 thread_local! {
     // A thread-local random number generator used for all randomness in a single thread. Not
     // threadsafe.
-    static THREAD_RNG: RefCell<Option<Rng>> = const { RefCell::new(None) };
+    static THREAD_RNG: RefCell<Option<Xoroshiro128Plus>> = const { RefCell::new(None) };
 }
 
 /// Initialize the global random number generator with a seed. This should only be called once
@@ -49,7 +50,7 @@ thread_local! {
 /// Returns an error if the random number generator has already been initialized.
 pub(crate) fn seed(seed: u64) -> Result<(), SlateDBError> {
     ROOT_RNG
-        .set(Mutex::new(Rng::with_seed(seed)))
+        .set(Mutex::new(Xoroshiro128Plus::seed_from_u64(seed)))
         .map_err(|_| SlateDBError::InvalidDBState)
 }
 
@@ -60,16 +61,16 @@ pub(crate) fn seed(seed: u64) -> Result<(), SlateDBError> {
 ///
 /// If the global random number generator hasn't been initialized, a default root random number
 /// generator will be created with a random seed.
-pub fn with_rng<T>(f: impl FnOnce(&mut Rng) -> T) -> T {
+pub fn with_rng<T>(f: impl FnOnce(&mut Xoroshiro128Plus) -> T) -> T {
     THREAD_RNG.with(|cell| {
         let mut maybe_rng = cell.borrow_mut();
         if maybe_rng.is_none() {
-            let rng = ROOT_RNG
-                .get_or_init(|| Mutex::new(Rng::new()))
+            let mut root_rng = ROOT_RNG
+                .get_or_init(|| Mutex::new(Xoroshiro128Plus::seed_from_u64(0)))
                 .lock()
-                .expect("mutex poisoned")
-                .fork();
-            *maybe_rng = Some(rng);
+                .expect("mutex poisoned");
+            let thread_local_rng = Xoroshiro128Plus::seed_from_u64(root_rng.next_u64());
+            *maybe_rng = Some(thread_local_rng);
         }
         f(maybe_rng.as_mut().expect("rng not initialized"))
     })
@@ -77,36 +78,38 @@ pub fn with_rng<T>(f: impl FnOnce(&mut Rng) -> T) -> T {
 
 #[cfg(test)]
 mod tests {
+    use rand_xoshiro::rand_core::RngCore;
+
     use super::*;
 
     // Helper to force a thread-local random number generator to use a specific seed so we
     // can test deterministic behavior.
     fn seed_local(seed: u64) {
         THREAD_RNG.with(|cell| {
-            cell.replace(Some(Rng::with_seed(seed)));
+            cell.replace(Some(Xoroshiro128Plus::seed_from_u64(seed)));
         });
     }
 
     #[test]
     fn test_rng() {
         seed_local(42);
-        let rand_u64 = with_rng(|rng| rng.u64(..));
-        assert_eq!(rand_u64, 14587678697106979209);
+        let rand_u64 = with_rng(|rng| rng.next_u64());
+        assert_eq!(rand_u64, 16629283624882167704);
     }
 
     #[test]
     fn test_rng_thread_local() {
         seed_local(42);
         with_rng(|outer_rng| {
-            let outer_u64 = outer_rng.u64(..);
+            let outer_u64 = outer_rng.next_u64();
             let inner_u64 = std::thread::spawn(move || {
                 seed_local(64);
-                with_rng(|inner_rng| inner_rng.u64(..))
+                with_rng(|inner_rng| inner_rng.next_u64())
             })
             .join()
             .unwrap();
-            assert_eq!(inner_u64, 13858685378923336244);
-            assert_eq!(outer_u64, 14587678697106979209);
+            assert_eq!(inner_u64, 18322729244133645280);
+            assert_eq!(outer_u64, 16629283624882167704);
         });
     }
 
