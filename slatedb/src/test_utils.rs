2357cb9326d61f5f597171d29dd8643e09ee5df1
Chris Riccomini <criccomini@apache.org>
2025-05-28 16:21:46 -0700

    Clean up misc usage in tests

--- slatedb/src/test_utils.rs
+++ slatedb/src/test_utils.rs
@@ -140,7 +140,7 @@ impl Clock for TokioClock {
 }
 
 pub(crate) fn gen_rand_bytes(n: usize) -> Bytes {
-    let mut rng = rand::thread_rng();
+    let mut rng = crate::rand::thread_rng();
     let random_bytes: Vec<u8> = (0..n).map(|_| rng.gen()).collect();
     Bytes::from(random_bytes)
 }
@@ -272,7 +272,7 @@ pub(crate) async fn seed_database(
 }
 
 pub(crate) fn build_test_sst(format: &SsTableFormat, num_blocks: usize) -> EncodedSsTable {
-    let mut rng = rand::thread_rng();
+    let mut rng = crate::rand::thread_rng();
     let mut keygen = OrderedBytesGenerator::new_with_suffix(&[], &[0u8; 16]);
     let mut encoded_sst_builder = format.table_builder();
     while encoded_sst_builder.num_blocks() < num_blocks {
