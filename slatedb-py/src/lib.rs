82fc9ec1006d60d19f95b1a5b4e9d466509ed430
Enwei Jiao <enwei.jiao@zilliz.com>
2025-06-23 14:38:34 +0800

    Add more functions to the Python binding (#627)
    Added the scan function, as the iterator pattern was too difficult to write, a simple version was implemented first.

--- slatedb-py/src/lib.rs
+++ slatedb-py/src/lib.rs
@@ -1,9 +1,11 @@
-use ::slatedb::object_store::{memory::InMemory, ObjectStore};
+use ::slatedb::admin::load_object_store_from_env;
+use ::slatedb::config::Settings;
+use ::slatedb::object_store::memory::InMemory;
 use ::slatedb::Db;
 use once_cell::sync::OnceCell;
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
-use pyo3::types::PyBytes;
+use pyo3::types::{PyBytes, PyDict, PyTuple};
 use pyo3_async_runtimes::tokio::future_into_py;
 use std::backtrace::Backtrace;
 use std::sync::Arc;
@@ -33,20 +35,59 @@ struct PySlateDB {
     db: Arc<Db>,
 }
 
+impl PySlateDB {
+    fn inner_get_bytes(&self, key: Vec<u8>) -> PyResult<Option<Vec<u8>>> {
+        if key.is_empty() {
+            return Err(create_value_error("key cannot be empty"));
+        }
+        let db = self.db.clone();
+        let rt = get_runtime();
+        rt.block_on(async {
+            match db.get(&key).await {
+                Ok(Some(bytes)) => Ok(Some(bytes.as_ref().to_vec())),
+                Ok(None) => Ok(None),
+                Err(e) => Err(create_value_error(e)),
+            }
+        })
+    }
+}
+
 #[pymethods]
 impl PySlateDB {
     #[new]
-    fn new(path: String) -> PyResult<Self> {
+    #[pyo3(signature = (path, env_file = None, *, **kwargs))]
+    fn new(
+        path: String,
+        env_file: Option<String>,
+        kwargs: Option<&Bound<PyDict>>,
+    ) -> PyResult<Self> {
         let rt = get_runtime();
-        let object_store: Arc<dyn ObjectStore> = Arc::new(InMemory::new());
+        let object_store = if let Some(env_file) = env_file {
+            load_object_store_from_env(Some(env_file)).map_err(create_value_error)?
+        } else {
+            Arc::new(InMemory::new())
+        };
         let db = rt.block_on(async {
-            Db::open(path.as_str(), object_store)
+            let settings = match kwargs.and_then(|k| k.get_item("settings").ok().flatten()) {
+                Some(settings_item) => {
+                    let settings_path = settings_item
+                        .extract::<String>()
+                        .map_err(create_value_error)?;
+                    Settings::from_file(settings_path).map_err(create_value_error)?
+                }
+                None => Settings::load().map_err(create_value_error)?,
+            };
+
+            Db::builder(path, object_store)
+                .with_settings(settings)
+                .build()
                 .await
                 .map_err(create_value_error)
         })?;
         Ok(Self { db: Arc::new(db) })
     }
 
+    #[pyo3(signature = (key, value))]
     fn put(&self, key: Vec<u8>, value: Vec<u8>) -> PyResult<()> {
         if key.is_empty() {
             return Err(create_value_error("key cannot be empty"));
@@ -57,17 +98,41 @@ impl PySlateDB {
     }
 
     fn get<'py>(&self, py: Python<'py>, key: Vec<u8>) -> PyResult<Option<Bound<'py, PyBytes>>> {
-        if key.is_empty() {
-            return Err(create_value_error("key cannot be empty"));
+        match self.inner_get_bytes(key)? {
+            Some(bytes) => Ok(Some(PyBytes::new(py, &bytes))),
+            None => Ok(None),
         }
+    }
+
+    #[pyo3(signature = (start, end = None))]
+    fn scan<'py>(
+        &self,
+        py: Python<'py>,
+        start: Vec<u8>,
+        end: Option<Vec<u8>>,
+    ) -> PyResult<Vec<Bound<'py, PyTuple>>> {
+        if start.is_empty() {
+            return Err(create_value_error("start cannot be empty"));
+        }
+        let start = start.clone();
+        let end = end.unwrap_or_else(|| {
+            let mut end = start.clone();
+            end.push(0xff);
+            end
+        });
+
         let db = self.db.clone();
         let rt = get_runtime();
         rt.block_on(async {
-            match db.get(&key).await {
-                Ok(Some(bytes)) => Ok(Some(PyBytes::new(py, &bytes))),
-                Ok(None) => Ok(None),
-                Err(e) => Err(create_value_error(e)),
+            let mut iter = db.scan(start..end).await.map_err(create_value_error)?;
+            let mut tuples = Vec::new();
+            while let Some(entry) = iter.next().await.map_err(create_value_error)? {
+                let key = PyBytes::new(py, &entry.key);
+                let value = PyBytes::new(py, &entry.value);
+                let tuple = PyTuple::new(py, vec![key, value])?;
+                tuples.push(tuple);
             }
+            Ok(tuples)
         })
     }
 
@@ -86,7 +151,6 @@ impl PySlateDB {
         rt.block_on(async { db.close().await.map_err(create_value_error) })
     }
 
-    // Async API methods
     fn put_async<'py>(
         &self,
         py: Python<'py>,
@@ -109,7 +173,7 @@ impl PySlateDB {
         let db = self.db.clone();
         future_into_py(py, async move {
             match db.get(&key).await {
-                Ok(Some(bytes)) => Ok(Some(bytes.to_vec())),
+                Ok(Some(bytes)) => Ok(Some(bytes.as_ref().to_vec())),
                 Ok(None) => Ok(None),
                 Err(e) => Err(create_value_error(e)),
             }
