913629303d51662f0cba26a573791f0ecd8a10a1
David Calavera <1050+calavera@users.noreply.github.com>
2025-07-14 11:01:59 -0700

    Add test and new metric for compacted bytes stored.
    The current metric of bytes compacted can be misleading when the compactor uses compression because it represents the bytes stored "after" compaction.
    
    I've added a new metric to separate both, bytes compacted "before" compression, and bytes stored "after" compaction.

--- slatedb/src/compactor_executor.rs
+++ slatedb/src/compactor_executor.rs
@@ -175,7 +175,9 @@ impl TokioCompactionExecutorInner {
         let mut current_writer = self
             .table_store
             .table_writer(SsTableId::Compacted(self.rand.rng().gen_ulid()));
-        let mut current_size = 0usize;
+
+        let mut raw_bytes_size = 0usize;
+        let mut compacted_block_size = 0usize;
         let mut total_bytes_processed = 0u64;
         let mut last_progress_report = self.clock.now();
 
@@ -228,25 +230,30 @@ impl TokioCompactionExecutorInner {
                 continue;
             }
 
-            let block_len = current_writer.add(kv).await?;
-            current_size += key_len + block_len;
+            let block_size = current_writer.add(kv).await?;
 
-            if current_size > self.options.max_sst_size {
-                current_size = 0;
+            compacted_block_size += key_len + block_size;
+            raw_bytes_size += key_len + value_len;
+
+            if compacted_block_size > self.options.max_sst_size {
+                compacted_block_size = 0;
                 let finished_writer = mem::replace(
                     &mut current_writer,
                     self.table_store
                         .table_writer(SsTableId::Compacted(self.rand.rng().gen_ulid())),
                 );
                 output_ssts.push(finished_writer.close().await?);
-                self.stats.bytes_compacted.add(current_size as u64);
+                self.stats.bytes_compacted.add(raw_bytes_size as u64);
+                self.stats.bytes_stored.add(compacted_block_size as u64);
             }
         }
 
-        if current_size > 0 {
+        if compacted_block_size > 0 {
             output_ssts.push(current_writer.close().await?);
-            self.stats.bytes_compacted.add(current_size as u64);
+            self.stats.bytes_compacted.add(raw_bytes_size as u64);
+            self.stats.bytes_stored.add(compacted_block_size as u64);
         }
+
         Ok(SortedRun {
             id: compaction.destination,
             ssts: output_ssts,
