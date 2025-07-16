ef7ccb7b712b55e8aa8599e7c11717eaab8728ef
Chris Riccomini <criccomini@apache.org>
2025-07-16 11:45:28 -0700

    Add more exhaustive determinism verification

--- slatedb-dst/tests/simulation.rs
+++ slatedb-dst/tests/simulation.rs
@@ -8,13 +8,10 @@ use tracing::error;
 use tracing::info;
 
 #[rstest]
-#[case(Rc::new(DbRand::new(1)), 10)]
-#[case(Rc::new(DbRand::new(2)), 10)]
-#[case(Rc::new(DbRand::new(3)), 10)]
-#[case(Rc::new(DbRand::new(4)), 10)]
-#[case(Rc::new(DbRand::new(5)), 10)]
+#[case(Rc::new(DbRand::new(1)), 100)]
+#[case(Rc::new(DbRand::new(2)), 100)]
 #[tokio::test(start_paused = true, flavor = "current_thread")]
-async fn test_dst_fast(
+async fn test_dst_quickly(
     #[case] rand: Rc<DbRand>,
     #[case] iterations: u32,
 ) -> Result<(), SlateDBError> {
@@ -32,8 +29,12 @@ async fn test_dst_fast(
 
 #[tokio::test(start_paused = true, flavor = "current_thread")]
 #[rstest]
-#[case(1, 10, 100)]
-async fn test_slatedb_is_deterministic(
+#[case(101, 10, 100)]
+#[case(102, 10, 100)]
+#[case(103, 10, 100)]
+#[case(104, 10, 100)]
+#[case(105, 10, 100)]
+async fn test_dst_is_deterministic(
     #[case] seed: u64,
     #[case] simulations: u32,
     #[case] iterations: u32,
@@ -46,8 +47,11 @@ async fn test_slatedb_is_deterministic(
             Ok(()) => {
                 let next_u64 = rand.rng().random::<u64>();
                 if let Some(expected_u64) = expected_u64 {
-                    eprintln!("expected_u64: {}, next_u64: {}", expected_u64, next_u64);
-                    assert_eq!(next_u64, expected_u64);
+                    assert_eq!(
+                        next_u64, expected_u64,
+                        "non-determinism detected: next_u64={}, expected_u64={}",
+                        next_u64, expected_u64
+                    );
                 }
                 expected_u64 = Some(next_u64);
             }
@@ -59,31 +63,3 @@ async fn test_slatedb_is_deterministic(
     }
     Ok(())
 }
-
-// TODO make test_deterministic_simulation have a seed parameter and use proptest
-// #[tokio::test(start_paused = true, flavor = "current_thread")]
-// async fn test_deterministic_simulation() -> Result<(), SlateDBError> {
-//     let seed = std::env::var("SLATEDB_DST_SEED")
-//         .map(|s| s.parse::<u64>().unwrap())
-//         .unwrap_or_else(|_| rand::random::<u64>());
-//     let rand = Rc::new(DbRand::new(seed));
-//     let db = build_db(&rand).await;
-//     let iterations = rand.rng().random_range(1..5_000_000);
-//     info!(seed, iterations, "test_deterministic_simulation");
-//     let dst_opts = DstOptions::default();
-//     match Dst::new(
-//         db,
-//         rand.clone(),
-//         Box::new(DefaultDstDistribution::new(rand, dst_opts.clone())),
-//         dst_opts,
-//     )
-//     .run_simulation(iterations)
-//     .await
-//     {
-//         Ok(_) => Ok(()),
-//         Err(e) => {
-//             error!(seed, ?e, "test_deterministic_simulation failed");
-//             Err(e)
-//         }
-//     }
-// }
