ab942df272834dec16f36da74990932bf87c94c1
Li Yazhou <me.ssword@gmail.com>
2025-07-27 22:59:28 +0800

    add scaffold to flush manifest about min retention seq

--- slatedb/src/transaction_manager.rs
+++ slatedb/src/transaction_manager.rs
@@ -1,7 +1,16 @@
+use crate::error::SlateDBError;
+use crate::utils::{spawn_bg_task, WatchableOnceCell, WatchableOnceCellReader};
 use parking_lot::RwLock;
 use std::collections::HashMap;
 use std::sync::Arc;
 use std::sync::Weak;
+use tokio::time::Instant;
+use tokio::{
+    select,
+    sync::mpsc::{self, error::TrySendError},
+    task::JoinHandle,
+};
+use tracing::{debug, error, instrument, trace};
 use uuid::Uuid;
 
 pub(crate) struct TransactionState {
@@ -13,10 +22,17 @@ pub(crate) struct TransactionState {
 pub struct TransactionManager {
     /// Map of transaction state ID to weak reference
     inner: Arc<RwLock<TransactionManagerInner>>,
+    quit_once: WatchableOnceCell<Result<(), SlateDBError>>,
 }
 
 struct TransactionManagerInner {
     active_txns: HashMap<Uuid, Weak<TransactionState>>,
+    /// The channel to send work to the background worker.
+    work_tx: Option<mpsc::Sender<TransactionBackgroundWork>>,
+    /// task handle of the background worker.
+    background_task: Option<JoinHandle<Result<(), SlateDBError>>>,
+    /// The last min retention seq that has been synced to the object store.
+    last_manifest_sync_time: Option<Instant>,
 }
 
 impl TransactionManager {
@@ -24,8 +40,42 @@ impl TransactionManager {
         Self {
             inner: Arc::new(RwLock::new(TransactionManagerInner {
                 active_txns: HashMap::new(),
+                work_tx: None,
+                background_task: None,
+                last_manifest_sync_time: None,
             })),
+            quit_once: WatchableOnceCell::new(),
         }
+    }
+
+    /// Start the background task for transaction management
+    pub async fn start_background(self: &Arc<Self>) -> Result<(), SlateDBError> {
+        if self.inner.read().background_task.is_some() {
+            return Err(SlateDBError::WalBufferAlreadyStarted);
+        }
+
+        let (work_tx, work_rx) = mpsc::channel(128);
+        {
+            let mut inner = self.inner.write();
+            inner.work_tx = Some(work_tx);
+        }
+
+        let background_fut = self
+            .clone()
+            .do_background_work(work_rx, self.quit_once.reader());
+        let self_clone = self.clone();
+        let task_handle = spawn_bg_task(
+            &tokio::runtime::Handle::current(),
+            move |result| {
+                Self::do_cleanup(self_clone, result.clone());
+            },
+            background_fut,
+        );
+        {
+            let mut inner = self.inner.write();
+            inner.background_task = Some(task_handle);
+        }
+        Ok(())
     }
 
     /// Register a transaction state with a specific ID
@@ -43,6 +93,18 @@ impl TransactionManager {
     pub fn remove_txn(&self, txn_state: &TransactionState) {
         let mut inner = self.inner.write();
         inner.active_txns.remove(&txn_state.id);
+
+        let need_sync_manifest = inner
+            .last_manifest_sync_time
+            .map(|t| t.elapsed().as_secs() > 10)
+            .unwrap_or(true);
+
+        if need_sync_manifest {
+            if let Some(tx) = self.inner.write().work_tx.as_ref() {
+                tx.try_send(TransactionBackgroundWork::SyncManifest).ok();
+            }
+            inner.last_manifest_sync_time = Some(Instant::now());
+        }
     }
 
     pub fn min_retention_seq(&self) -> Option<u64> {
@@ -52,5 +114,54 @@ impl TransactionManager {
             .values()
             .filter_map(|state| state.upgrade().map(|state| state.seq))
             .min()
+    }
+
+    async fn do_background_work(
+        self: Arc<Self>,
+        mut work_rx: mpsc::Receiver<TransactionBackgroundWork>,
+        mut quit_rx: WatchableOnceCellReader<Result<(), SlateDBError>>,
+    ) -> Result<(), SlateDBError> {
+        loop {
+            select! {
+                work = work_rx.recv() => {
+                    match work {
+                        None => break,
+                        Some(work) => {
+                            match work {
+                                TransactionBackgroundWork::SyncManifest => {
+                                    // TODO: sync manifest to the object store
+                                }
+                            }
+                        }
+                    }
+                }
+                _ = quit_rx.await_value() => {
+                    return Ok(());
+                }
+            };
+        }
+
+        Ok(())
+    }
+
+    fn do_cleanup(self: Arc<Self>, result: Result<(), SlateDBError>) {
+        if let Err(e) = &result {
+            self.quit_once.write(Err(e.clone()));
+        }
+    }
+
+    pub async fn close(&self) -> Result<(), SlateDBError> {
+        self.quit_once.write(Ok(()));
+        Ok(())
+    }
+}
+
+enum TransactionBackgroundWork {
+    SyncManifest,
+}
+
+impl std::fmt::Debug for TransactionBackgroundWork {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("TransactionBackgroundWork").finish()
     }
 }
