48a6c1aeb5b51b2c126df8d948ddfe79796ae8db
Chris Riccomini <criccomini@apache.org>
2025-09-03 14:19:07 -0700

    Default to 5 minutes of retries before failing

--- slatedb/src/manifest/store.rs
+++ slatedb/src/manifest/store.rs
@@ -583,7 +583,7 @@ impl ManifestStore {
             self.write_manifest_in_object_store(manifest_path, manifest)
                 .await
         })
-        .retry(ExponentialBuilder::default().with_max_times(usize::MAX))
+        .retry(ExponentialBuilder::default().with_max_times(10)) // ~5 minutes w/default settings
         .when(utils::should_retry_object_store_operation)
         .await
     }
