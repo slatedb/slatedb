f0ec49a173265159942ffec6dde113a0b6428dde
Victor Ordaz <vito.ordaz@gmail.com>
2025-11-27 11:30:42 -0800

    use pattern matching

--- slatedb-go/src/object_store.rs
+++ slatedb-go/src/object_store.rs
@@ -1,3 +1,4 @@
+use crate::error::{create_error_result, CSdbError};
 use object_store::memory::InMemory;
 use slatedb::admin::load_object_store_from_env;
 use slatedb::object_store::ObjectStore;
@@ -9,22 +10,19 @@ pub fn create_object_store(
     url: Option<&str>,
     env_file: Option<String>,
 ) -> Result<Arc<dyn ObjectStore>, crate::error::CSdbResult> {
-    use crate::error::{create_error_result, CSdbError};
-    if let Some(url) = url {
-        return Db::resolve_object_store(url).map_err(|e| {
+    match (url, env_file) {
+        (Some(url), _) => Db::resolve_object_store(url).map_err(|e| {
             create_error_result(
                 CSdbError::InternalError,
                 &format!("Failed to resolve object store: {}", e),
             )
-        });
-    }
-    if let Some(env) = env_file {
-        return load_object_store_from_env(Some(env)).map_err(|e| {
+        }),
+        (None, Some(env_file)) => load_object_store_from_env(Some(env_file)).map_err(|e| {
             create_error_result(
                 CSdbError::InternalError,
                 &format!("Failed to load object store from environment: {}", e),
             )
-        });
+        }),
+        (None, None) => Ok(Arc::new(InMemory::new())),
     }
-    Ok(Arc::new(InMemory::new()))
 }
