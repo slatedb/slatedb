6b606468ea6518eec7b34278cc93a9016740f4c3
Chris Riccomini <criccomini@apache.org>
2026-03-14 10:43:47 -0700

    Add from_env

--- bindings/ffi/src/object_store.rs
+++ bindings/ffi/src/object_store.rs
@@ -1,8 +1,8 @@
 use std::sync::Arc;
 
-use slatedb::Db;
+use slatedb::{admin, Db};
 
-use crate::error::FfiError;
+use crate::error::{FfiError, FfiSlateDbError};
 
 #[derive(uniffi::Object)]
 pub struct FfiObjectStore {
@@ -14,6 +14,12 @@ impl FfiObjectStore {
     #[uniffi::constructor]
     pub fn resolve(url: String) -> Result<Arc<Self>, FfiError> {
         let inner = Db::resolve_object_store(&url)?;
+        Ok(Arc::new(Self { inner }))
+    }
+
+    #[uniffi::constructor]
+    pub fn from_env(env_file: Option<String>) -> Result<Arc<Self>, FfiError> {
+        let inner = admin::load_object_store_from_env(env_file).map_err(FfiSlateDbError::from)?;
         Ok(Arc::new(Self { inner }))
     }
 }
