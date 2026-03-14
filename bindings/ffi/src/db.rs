bcbb359844df5666ef9055c9e30c35f3b769f672
Chris Riccomini <criccomini@apache.org>
2026-03-13 20:16:40 -0700

    Move to db_snapshot/db_transaction

--- bindings/ffi/src/db.rs
+++ bindings/ffi/src/db.rs
@@ -5,9 +5,10 @@ use crate::config::{
     FfiFlushOptions, FfiIsolationLevel, FfiMergeOptions, FfiPutOptions, FfiReadOptions,
     FfiScanOptions, FfiWriteOptions,
 };
+use crate::db_snapshot::FfiDbSnapshot;
+use crate::db_transaction::FfiDbTransaction;
 use crate::error::FfiError;
 use crate::iterator::FfiDbIterator;
-use crate::transaction::FfiDbTransaction;
 use crate::types::{FfiKeyRange, FfiKeyValue, FfiWriteHandle};
 use crate::validation::{validate_key, validate_key_value};
 use crate::write_batch::FfiWriteBatch;
@@ -217,9 +218,7 @@ impl FfiDb {
     }
 
     pub async fn snapshot(&self) -> Result<Arc<FfiDbSnapshot>, FfiError> {
-        Ok(Arc::new(FfiDbSnapshot {
-            inner: self.inner.snapshot().await?,
-        }))
+        Ok(Arc::new(FfiDbSnapshot::new(self.inner.snapshot().await?)))
     }
 
     pub async fn begin(
@@ -228,89 +227,5 @@ impl FfiDb {
     ) -> Result<Arc<FfiDbTransaction>, FfiError> {
         let tx = self.inner.begin(isolation_level.into_core()).await?;
         Ok(Arc::new(FfiDbTransaction::new(tx)))
-    }
-}
-
-#[derive(uniffi::Object)]
-pub struct FfiDbSnapshot {
-    inner: Arc<slatedb::DbSnapshot>,
-}
-
-#[uniffi::export(async_runtime = "tokio")]
-impl FfiDbSnapshot {
-    pub async fn get(&self, key: Vec<u8>) -> Result<Option<Vec<u8>>, FfiError> {
-        Ok(self.inner.get(key).await?.map(|value| value.to_vec()))
-    }
-
-    pub async fn get_with_options(
-        &self,
-        key: Vec<u8>,
-        options: FfiReadOptions,
-    ) -> Result<Option<Vec<u8>>, FfiError> {
-        let options = options.into_core();
-        Ok(self
-            .inner
-            .get_with_options(key, &options)
-            .await?
-            .map(|value| value.to_vec()))
-    }
-
-    pub async fn get_key_value(&self, key: Vec<u8>) -> Result<Option<FfiKeyValue>, FfiError> {
-        Ok(self
-            .inner
-            .get_key_value(key)
-            .await?
-            .map(FfiKeyValue::from_core))
-    }
-
-    pub async fn get_key_value_with_options(
-        &self,
-        key: Vec<u8>,
-        options: FfiReadOptions,
-    ) -> Result<Option<FfiKeyValue>, FfiError> {
-        let options = options.into_core();
-        Ok(self
-            .inner
-            .get_key_value_with_options(key, &options)
-            .await?
-            .map(FfiKeyValue::from_core))
-    }
-
-    pub async fn scan(&self, range: FfiKeyRange) -> Result<Arc<FfiDbIterator>, FfiError> {
-        let range = range.into_bounds()?;
-        let iter = self.inner.scan::<Vec<u8>, _>(range).await?;
-        Ok(Arc::new(FfiDbIterator::new(iter)))
-    }
-
-    pub async fn scan_with_options(
-        &self,
-        range: FfiKeyRange,
-        options: FfiScanOptions,
-    ) -> Result<Arc<FfiDbIterator>, FfiError> {
-        let range = range.into_bounds()?;
-        let options = options.into_core()?;
-        let iter = self
-            .inner
-            .scan_with_options::<Vec<u8>, _>(range, &options)
-            .await?;
-        Ok(Arc::new(FfiDbIterator::new(iter)))
-    }
-
-    pub async fn scan_prefix(&self, prefix: Vec<u8>) -> Result<Arc<FfiDbIterator>, FfiError> {
-        let iter = self.inner.scan_prefix(prefix).await?;
-        Ok(Arc::new(FfiDbIterator::new(iter)))
-    }
-
-    pub async fn scan_prefix_with_options(
-        &self,
-        prefix: Vec<u8>,
-        options: FfiScanOptions,
-    ) -> Result<Arc<FfiDbIterator>, FfiError> {
-        let options = options.into_core()?;
-        let iter = self
-            .inner
-            .scan_prefix_with_options(prefix, &options)
-            .await?;
-        Ok(Arc::new(FfiDbIterator::new(iter)))
     }
 }
--- bindings/ffi/src/lib.rs
+++ bindings/ffi/src/lib.rs
@@ -2,12 +2,13 @@ mod builder;
 mod config;
 mod db;
 mod db_reader;
+mod db_snapshot;
+mod db_transaction;
 mod error;
 mod iterator;
 mod merge_operator;
 mod object_store;
 mod settings;
-mod transaction;
 mod types;
 mod validation;
 mod wal;
@@ -19,14 +20,15 @@ pub use config::{
     FfiPutOptions, FfiReadOptions, FfiReaderOptions, FfiScanOptions, FfiSstBlockSize, FfiTtl,
     FfiWriteOptions,
 };
-pub use db::{FfiDb, FfiDbSnapshot};
+pub use db::FfiDb;
 pub use db_reader::FfiDbReader;
+pub use db_snapshot::FfiDbSnapshot;
+pub use db_transaction::FfiDbTransaction;
 pub use error::{FfiCloseReason, FfiError, FfiMergeOperatorCallbackError};
 pub use iterator::FfiDbIterator;
 pub use merge_operator::FfiMergeOperator;
 pub use object_store::FfiObjectStore;
 pub use settings::FfiSettings;
-pub use transaction::FfiDbTransaction;
 pub use types::{FfiKeyRange, FfiKeyValue, FfiRowEntry, FfiRowEntryKind, FfiWriteHandle};
 pub use wal::{FfiWalFile, FfiWalFileIterator, FfiWalFileMetadata, FfiWalReader};
 pub use write_batch::FfiWriteBatch;
