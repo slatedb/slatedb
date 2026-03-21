89741e939dbc507f30d9b1709aadbf16ba538ad9
Chris <criccomini@users.noreply.github.com>
2026-03-21 10:09:46 -0700

    Remove generated Java code (#1421)

--- bindings/java/slatedb-uniffi/src/generated/java/io/slatedb/uniffi/UniffiForeignFutureCompleteU16.java
+++
@@ -1,9 +0,0 @@
-package io.slatedb.uniffi;
-
-
-import com.sun.jna.*;
-import com.sun.jna.ptr.*;
-
-interface UniffiForeignFutureCompleteU16 extends Callback {
-    public void callback(long callbackData,UniffiForeignFutureStructU16.UniffiByValue result);
-}
--- bindings/java/slatedb-uniffi/src/generated/java/io/slatedb/uniffi/UniffiForeignFutureCompleteU32.java
+++
@@ -1,9 +0,0 @@
-package io.slatedb.uniffi;
-
-
-import com.sun.jna.*;
-import com.sun.jna.ptr.*;
-
-interface UniffiForeignFutureCompleteU32 extends Callback {
-    public void callback(long callbackData,UniffiForeignFutureStructU32.UniffiByValue result);
-}
