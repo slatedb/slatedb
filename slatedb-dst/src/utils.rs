92bd60c650cf20611cc38a562d9d89c420ca0249
Ryan Dielhenn <dielhennr@gmail.com>
2026-06-10 00:53:38 -0700

    extend commit_compacted_interval range in dst

--- slatedb-dst/src/utils.rs
+++ slatedb-dst/src/utils.rs
@@ -116,7 +116,7 @@ pub fn build_settings_compactor(rng: &mut impl Rng) -> CompactorOptions {
         }),
         metric_level: None,
         commit_compacted_interval: rng
-            .random_range(Duration::from_millis(1)..Duration::from_secs(1)),
+            .random_range(Duration::from_millis(1)..Duration::from_secs(3)),
     }
 }
 
